2ed9c919dbc6ec5194744bac565d8a0f0c08bdf5
James <jimmyvelarde3302@gmail.com>
2022-02-20 02:50:00 -0700

    Fixed delete functionality

--- public/javascript/delete.js
+++ public/javascript/delete.js
@@ -1,9 +1,10 @@
 const deletePostHandler = async function(event) {
+    console.log("clicked", event)
     event.preventDefault();
+    const postId = document.getElementById('post-id')
 
     fetch("/api/post/" + postId.value, {
-        method: "delete",
-        // headers: { "Content-Type": "application/json"}
+        method: "delete"
     })
     .then(function() {
         document.location.replace("/dashboard");
@@ -11,4 +12,4 @@ const deletePostHandler = async function(event) {
     .catch(err => console.log(err))
 }
 
-document.querySelector("#delete-btn").addEventListener("button", deletePostHandler)
+document.querySelector("#delete-btn").addEventListener("click", deletePostHandler);
